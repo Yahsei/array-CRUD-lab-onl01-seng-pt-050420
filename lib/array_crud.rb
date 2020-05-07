d6eb0d50ceafeb423f3fb78214dc7902a2bf2445
Jesse Summer-Lavigne <jessesl22@gmail.com>
2020-05-07 03:53:59 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -22,7 +22,7 @@ def remove_element_from_end_of_array(array)
 end
 
 def remove_element_from_start_of_array(array)
-   numbers = ['wow', 2, 3, 4, "arrays!"]
+   numbers = ["wow", 2, 3, 4, "arrays!"]
    numbers.unshift
 end
 
