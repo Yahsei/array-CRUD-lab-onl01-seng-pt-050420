ee45a5b32739410be9b15f4c6e6bb168cb079437
Jesse Summer-Lavigne <jessesl22@gmail.com>
2020-05-07 03:49:01 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -13,7 +13,7 @@ def add_element_to_end_of_array(array, element)
 end
 
 def add_element_to_start_of_array(array, element)
- numbers = [1, 2, 3, 4]
+ numbers = ["hey", "hi"]
  numbers.unshift("wow")
 
 def remove_element_from_end_of_array(array)
