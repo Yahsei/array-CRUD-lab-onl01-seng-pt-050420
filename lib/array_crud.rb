cabf64517032cbd2b2ce436342c43f8b77a17526
Jesse Summer-Lavigne <jessesl22@gmail.com>
2020-05-07 03:48:02 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -14,7 +14,7 @@ end
 
 def add_element_to_start_of_array(array, element)
  numbers = [1, 2, 3, 4]
-  numbers.unshift(wow)
+ numbers.unshift("wow")
 
 def remove_element_from_end_of_array(array)
   
