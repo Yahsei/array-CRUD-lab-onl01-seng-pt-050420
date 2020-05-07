27b6104955bb5d457e6a0ed41e842be99ada582c
Jesse Summer-Lavigne <jessesl22@gmail.com>
2020-05-07 04:06:03 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -13,7 +13,7 @@ def add_element_to_end_of_array(array, element)
 end
 
 def add_element_to_start_of_array(array, element)
-wat = ["ah", "um"]
+
 wat.unshift("wow")
 
 def remove_element_from_end_of_array(array)
